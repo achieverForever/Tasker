8a5a3c2e8b25ecd4ff264500f6efe34d74e54e83
Wilson Kwok <achiever4ever@hotmail.com>
2015-03-30 16:21:21 +0800

    Add TestAction for debugging.

---
+++ app/src/main/java/com/wilson/tasker/actions/TestAction.java
@@ -0,0 +1,24 @@
+package com.wilson.tasker.actions;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.wilson.tasker.model.Action;
+
+public class TestAction extends Action {
+	@Override
+	public boolean performAction(Context context) {
+		Toast.makeText(context, "Test action activated!", Toast.LENGTH_LONG).show();
+		return true;
+	}
+
+	@Override
+	public View getView(Context context, ViewGroup parent) {
+		TextView view = new TextView(context);
+		view.setText("Test Action");
+		return view;
+	}
+}
