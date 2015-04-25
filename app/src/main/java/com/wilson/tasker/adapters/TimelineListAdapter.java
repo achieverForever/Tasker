f3da69e531fb86fed149a36ad724afd4b3305c39
Wilson Kwok <achiever4ever@hotmail.com>
2015-04-25 09:39:07 +0800

    Optimize timeline view.

--- app/src/main/java/com/wilson/tasker/adapters/TimelineListAdapter.java
+++ app/src/main/java/com/wilson/tasker/adapters/TimelineListAdapter.java
@@ -12,8 +12,11 @@ import com.wilson.tasker.dao.SceneActivity;
 import com.wilson.tasker.manager.FontManager;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class TimelineListAdapter extends BaseAdapter {
 
@@ -54,11 +57,25 @@ public class TimelineListAdapter extends BaseAdapter {
 		TextView detailInfo = (TextView) convertView.findViewById(R.id.detail_info);
 
 		String activated = item.getActionType() == 0 ? "activated" : "deactivated";
-		time.setText(new SimpleDateFormat("H:m").format(item.getTime()));
+		if (isRecentTime(item.getTime())) {
+			time.setText(new SimpleDateFormat("EEE H:m").format(item.getTime()));
+		} else {
+			long days = TimeUnit.DAYS.convert(new Date().getTime() - item.getTime().getTime(),
+					TimeUnit.MILLISECONDS);
+			time.setText(String.format("%d天前 ", days) +
+					new SimpleDateFormat("H:m").format(item.getTime()));
+		}
 		info.setText("Scene " + activated);
 		detailInfo.setText("Scene " + item.getSceneName()+ " is " + activated);
 		detailInfo.setTypeface(FontManager.getsInstance().loadFont(context, "fonts/Roboto-Light.ttf"));
 
 		return convertView;
+	}
+
+	private boolean isRecentTime(Date time) {
+		Calendar calendar = Calendar.getInstance();
+		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+		long days = TimeUnit.DAYS.convert(calendar.getTime().getTime() - time.getTime(), TimeUnit.MILLISECONDS);
+		return days < dayOfWeek;
 	}
 }
