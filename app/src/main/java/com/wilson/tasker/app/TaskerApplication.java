5007cb504f2b1039ed035ae5f3c603920d460cb8
Wilson Kwok <achiever4ever@hotmail.com>
2015-04-27 19:42:48 +0800

    Setup several default scenes.

--- app/src/main/java/com/wilson/tasker/app/TaskerApplication.java
+++ app/src/main/java/com/wilson/tasker/app/TaskerApplication.java
@@ -3,15 +3,23 @@ package com.wilson.tasker.app;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 
 import com.baidu.mapapi.SDKInitializer;
 import com.wilson.tasker.actions.BluetoothAction;
 import com.wilson.tasker.actions.BrightnessAction;
+import com.wilson.tasker.actions.RingerModeAction;
+import com.wilson.tasker.actions.WallpaperAction;
+import com.wilson.tasker.actions.WifiAction;
+import com.wilson.tasker.actions.WifiConnectAction;
 import com.wilson.tasker.conditions.BatteryLevelCondition;
+import com.wilson.tasker.conditions.LocationCondition;
+import com.wilson.tasker.conditions.OrientationCondition;
 import com.wilson.tasker.conditions.SmsCondition;
 import com.wilson.tasker.conditions.TopAppCondition;
 import com.wilson.tasker.dao.DaoMaster;
 import com.wilson.tasker.dao.DaoSession;
+import com.wilson.tasker.manager.OrientationManager;
 import com.wilson.tasker.manager.SceneManager;
 import com.wilson.tasker.model.Action;
 import com.wilson.tasker.model.Condition;
@@ -32,55 +40,36 @@ public class TaskerApplication extends Application {
 		SDKInitializer.initialize(this);
 		startService(new Intent(this, WorkerService.class));
 
-		//test();
 		if (Utils.isFreshInstall(this)) {
 			initDefaultScenes();
 		} else {
-			SceneManager.getInstance()
-					.loadScenes(this, getSharedPreferences(WorkerService.SHARED_PREF_NAME, 0));
+			SceneManager.getInstance().loadScenes(this,
+					getSharedPreferences(WorkerService.SHARED_PREF_NAME, 0));
 		}
 
 	}
 
 	private void initDefaultScenes() {
-		// TODO - 默认的几个Scene
-//		batteryConditions.add(new TopAppCondition("com.android.chrome"));
-//		batteryConditions.add(new BatteryLevelCondition(BatteryLevelCondition.BatteryLevelType.ABOVE, 0.8f));
-//		batteryConditions.add(new TopAppCondition("com.android.chrome"));
-//		batteryConditions.add(new CallerCondition("123"));
+		Scene battery = new Scene("battery", "Battery", true);
+		battery.addCondition(new BatteryLevelCondition(BatteryLevelCondition.TYPE_BELOW, 0.2f));
+		battery.addAction(new BluetoothAction(false));
+		battery.addAction(new BrightnessAction(64));
+		battery.addAction(new WifiAction(false));
 
-		Scene batteryScene = new Scene("battery", "Battery", false);
-		batteryScene.addCondition(new SmsCondition("10657301", "hi"));
-//		batteryScene.addCondition(new CallerCondition("26888"));
-//		batteryScene.addAction(new BluetoothAction(true));
-//		batteryScene.addAction(new WifiAction(true));
+		Scene home = new Scene("home", "Home", true);
+		home.addCondition(new LocationCondition("Home"));
+		home.addAction(new WifiConnectAction(0, "home"));
+		home.addAction(new BrightnessAction(194));
 
-		Scene homeScene = new Scene("home", "Home", false);
-		Scene sleepScene = new Scene("sleep", "Sleep", false);
+		Scene meeting = new Scene("meeting", "Meeting", true);
+		meeting.addCondition(new OrientationCondition(OrientationManager.ORIENTATION_FACE_DOWN));
+		meeting.addAction(new RingerModeAction(AudioManager.RINGER_MODE_SILENT));
 
-		SceneManager.getInstance().addScene(this, batteryScene);
-		SceneManager.getInstance().addScene(this, homeScene);
-		SceneManager.getInstance().addScene(this, sleepScene);
-		SceneManager.getInstance()
-				.saveScenes(getSharedPreferences(WorkerService.SHARED_PREF_NAME, 0));
-
-	}
-
-	private void test() {
-
-		BatteryLevelCondition condition = new BatteryLevelCondition(BatteryLevelCondition.TYPE_ABOVE, 100);
-		String streamCondition = Utils.GSON.toJson(condition, Condition.class);
-		BatteryLevelCondition condition2 = (BatteryLevelCondition) Utils.GSON.fromJson(streamCondition, Condition.class);
-
-		BrightnessAction action = new BrightnessAction(100);
-		String streamAction = Utils.GSON.toJson(action, Action.class);
-		BrightnessAction action2 = (BrightnessAction) Utils.GSON.fromJson(streamAction, Action.class);
-
-		Scene batteryScene = new Scene("battery", "Battery", false);
-		batteryScene.addCondition(new TopAppCondition("com.android.chrome"));
-		batteryScene.addAction(new BluetoothAction(true));
-		String streamScene = Utils.GSON.toJson(batteryScene, Scene.class);
-		Scene scene2 = Utils.GSON.fromJson(streamScene, Scene.class);
+		SceneManager.getInstance().addScene(this, battery);
+		SceneManager.getInstance().addScene(this, home);
+		SceneManager.getInstance().addScene(this, meeting);
+		SceneManager.getInstance().saveScenes(getSharedPreferences(
+				WorkerService.SHARED_PREF_NAME, 0));
 
 	}
 
